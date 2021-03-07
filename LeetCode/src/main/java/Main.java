eeeda83490810aebdc34bbdcf30aa34b033ed4b9
vasquez_d <vasquezd@oregonstate.edu>
2021-03-06 17:38:37 -0800

    updates

--- LeetCode/src/main/java/Main.java
+++ LeetCode/src/main/java/Main.java
@@ -1,5 +1,35 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
+        //String s =  "abcabcbb";
+        String s =  "abac";
+        int longestString = Integer.MIN_VALUE;
+        Set<Character> uniqueCharSet = new HashSet<Character>();
+        ArrayList<Character> charArray = new ArrayList<Character>();
+
+        for (int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+
+            //Add Character to the set and list
+            uniqueCharSet.add(c);
+            charArray.add(c);
+
+            //Compare the List and the Set
+            if(uniqueCharSet.size() != charArray.size()) {
+                System.out.println("Not Unique " + charArray + " | " + uniqueCharSet);
+            } else {
+                System.out.println("Still Unique " + charArray);
+            }
+
+
+
+            //System.out.println(c);
+
+        }
+
 
     }
 }
