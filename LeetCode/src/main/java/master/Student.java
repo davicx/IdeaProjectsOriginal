9b87a6d0504b327b3ef96a21da093e2b82f3adb0
vasquez_d <vasquezd@oregonstate.edu>
2021-03-09 17:11:32 -0800

    updates

---
+++ LeetCode/src/main/java/master/Student.java
@@ -0,0 +1,48 @@
+package master;
+public class Student implements Comparable<Student> {
+    private int id;
+    private String name;
+    private int grade;
+    private double gpa;
+
+    public Student(int id, String name, int grade, double gpa) {
+        this.id   = id;
+        this.name    = name;
+        this.grade   = grade;
+        this.gpa = gpa;
+    }
+
+    public int getID() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+    public int getGrade() {
+        return grade;
+    }
+    public double getGPA() {
+        return gpa;
+    }
+
+    public boolean equals(Student other) {
+        return this.getID() == other.getID();
+    }
+
+    public int compareTo(Student other) {
+        if(this.equals(other)) {
+            return 0;
+        } else if (getID() > other.getID()) {
+            return 1;
+        } else {
+            return -1;
+        }
+    }
+
+    public String toString() {
+        return getID() + " " + getName() + " " + getGrade() + " " + getGPA();
+    }
+
+
+
+}
--- LeetCode/src/main/java/master/priorityQueueMap.java
+++ LeetCode/src/main/java/master/priorityQueueMap.java
@@ -1,4 +1,70 @@
 package master;
 
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+
 public class priorityQueueMap {
+
+    public static void main(String[] args) {
+        priorityQueue();
+
+
+
+    }
+
+    public static void priorityQueue() {
+        PriorityQueue<Integer> myQueue = new PriorityQueue<Integer>();
+        Student david = new Student(1,"david", 14, 3.9);
+        System.out.println(david.toString());
+
+        /*
+        PriorityQueue<String> myQueue = new PriorityQueue<String>();
+        myQueue.add("b");
+        myQueue.add("c");
+        myQueue.add("a");
+        myQueue.add("d");
+
+        myQueue.add(1);
+        myQueue.add(8);
+        myQueue.add(3);
+        myQueue.add(2);
+
+        while(!myQueue.isEmpty()) {
+            System.out.println(myQueue);
+            System.out.println(myQueue.poll());
+        }
+        */
+
+
+    }
+
+    public static void priorityQueueExample() {
+        //Queue<E> queue = new PriorityQueue<E>(c);
+
+
+    }
+
+
+
+    public static void normalQueue() {
+        Queue<Integer> nums = new LinkedList<Integer>();
+        nums.offer(1); //Prevent errors returns true or false
+        nums.add(2);
+        nums.add(3);
+        nums.add(4);
+        //System.out.println(nums.peek());
+        //System.out.println(nums.element());
+        //System.out.println();
+        while(nums.size() > 0) {
+            //System.out.println(nums.remove());
+            System.out.println(nums.poll());
+        }
+        System.out.println(nums.poll());
+        System.out.println(nums.peek());
+
+    }
+
+
 }
